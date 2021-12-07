3ee9a215ca230e9797526b293e4c03405096ba0f
Pierre litoux <pierre.litoux@epita.fr>
2021-12-07 19:24:08 +0100

    var_expansion : uses now setenv and getenv

--- src/var_expansion/var_expansion.c
+++ src/var_expansion/var_expansion.c
@@ -5,44 +5,13 @@
 #include <lexer/token.h>
 #include <utils/alloc.h>
 #include <utils/vec.h>
+#include <stdlib.h>
 #define HASH_MAP_SIZE 32
 
-/**
- * @brief init the hash_map storing our variables
- *
- * @return struct hash_map*
- */
-struct hash_map *var_hash_map_init(void)
+static bool is_a_spec_char(char c)
 {
-    struct hash_map *hash_map = hash_map_init(HASH_MAP_SIZE);
-    return hash_map;
-}
-
-/**
- * @brief insert a new pair value / key in the hash map
- * key pair syntax : var=true
- * key : var
- * value : true
- * => $(var) = true
- * @param hash_map
- * @param key name of the var
- * @param value value inside
- * @return true
- * @return false
- */
-bool var_hash_map_insert(struct hash_map *hash_map, char *key, char *value)
-{
-    return hash_map_insert(hash_map, key, value, free);
-}
-
-/**
- * @brief free an hash map full of variables
- *
- * @param hash_map
- */
-void var_hash_map_free(struct hash_map *hash_map)
-{
-    hash_map_free(hash_map, free);
+    bool res = c == '$' || c == '@' || c == '?' || c == '*' || c == '#';
+    return res;
 }
 
 /**
@@ -58,7 +27,7 @@ static bool is_separator(char c, int count)
     bool res = true;
     if (count <= 1)
     {
-        res = !isalnum(c) && c != '$' && c != '{' && c != '_';
+        res = !isalnum(c) && !is_a_spec_char(c) && c != '{' && c != '_';
     }
     else
     {
@@ -74,7 +43,7 @@ static bool is_separator(char c, int count)
 int check_valid_substitution(char *str, size_t count)
 {
     int res = 1;
-    if ((str[0] == '$' || isdigit(str[0])) && count > 3)
+    if ((is_a_spec_char(str[0]) || isdigit(str[0])) && count > 3)
     {
         return 0;
     }
@@ -240,26 +209,27 @@ char *build_key(char *var, int *error, size_t *counter)
         return res;
     }
 }
-
+#include <stdio.h>
 /**
  * @brief return the value associated to this var,
  * key pair syntax : var=true,
  * key : var,
  * value : true,
  * => ${var} = true
- * @param hash_map
  * @param var
  * @return char*
  */
-char *expand_var(struct hash_map *hash_map, char *var, int *error,
+char *expand_var(char *var, int *error,
                  size_t *counter)
 {
-    if (var == NULL || hash_map == NULL || var[0] != '$')
+    if (var == NULL || var[0] != '$')
+    {
         return NULL;
+    }
     char *key = build_key(var, error, counter);
     if (key == NULL)
         return NULL;
-    char *value = hash_map_get(hash_map, key);
+    char *value = getenv(key);
     free(key);
     if (value == NULL)
         return "";
@@ -285,11 +255,10 @@ static int can_be_escaped(char c)
  * key : var,
  * value : true,
  * => ${var} = true
- * @param hash_map
  * @param var
  * @return char*
  */
-char *expand_all_string(struct hash_map *hash_map, char *str, int *error,
+char *expand_all_string(char *str, int *error,
                         size_t *counter)
 {
     if (str[0] != '\"')
@@ -322,7 +291,7 @@ char *expand_all_string(struct hash_map *hash_map, char *str, int *error,
             break;
         }
         // else it's a word
-        char *word = expand_var(hash_map, str + count, error, &count);
+        char *word = expand_var(str + count, error, &count);
         /*printf("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa\n");
         if (word != NULL)
             printf("%s\n", word);
